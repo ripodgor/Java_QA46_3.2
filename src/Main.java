d71d8dff2e6b19c66278fa4de3bb79dd8a18c854
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-09-19 14:25:00 +0300

    change files

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
-        public static void main(String[] args) {
-            int initialAmount = 500; // начальная сумма счёта
-            int minimumAmount = 1000; // минимальная сумма для выполнения условия акции
-            int depositAmount = 10_423; // сумма пополнения
-            int bonusConditions = 100; // сотовый оператор дарит по 1 рублю за каждые полные 100 рублей пополнения
-            int bonus = depositAmount / bonusConditions; // сумма бонуса
-            if (depositAmount < minimumAmount) {
-                bonus = 0; }
-            int balance = depositAmount + initialAmount + bonus; // всего на балансе
-            System.out.println("За пополнение счёта вам зачислено " + bonus + " руб! Всего на балансе " + balance + " руб.");
+    public static void main(String[] args) {
+        int initialAmount = 500; // начальная сумма счёта
+        int minimumAmount = 1000; // минимальная сумма для выполнения условия акции
+        int depositAmount = 10_423; // сумма пополнения
+        int bonusConditions = 100; // сотовый оператор дарит по 1 рублю за каждые полные 100 рублей пополнения
+        int bonus = depositAmount / bonusConditions; // сумма бонуса
+        if (depositAmount < minimumAmount) {
+            bonus = 0;
         }
+        int balance = depositAmount + initialAmount + bonus; // всего на балансе
+        System.out.println("За пополнение счёта вам зачислено " + bonus + " руб! Всего на балансе " + balance + " руб.");
     }
+}
